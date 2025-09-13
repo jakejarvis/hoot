969f5800151ffdc776acc7b39bcdf308b0c99c14
Jake Jarvis <jake@jarv.is>
2025-09-13 07:50:48 -0400

    Integrate superjson transformer in TRPCProvider for enhanced data serialization in API requests.

--- components/trpc-provider.tsx
+++ components/trpc-provider.tsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { trpc } from "@/lib/trpc/client"
 import { httpBatchStreamLink, loggerLink } from "@trpc/client"
+import superjson from "superjson"
 
 export function TRPCProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient())
@@ -11,7 +12,7 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         loggerLink({ enabled: () => process.env.NODE_ENV === "development" }),
-        httpBatchStreamLink({ url: "/api/trpc" }),
+        httpBatchStreamLink({ url: "/api/trpc", transformer: superjson }),
       ],
     })
   )
