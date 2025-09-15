4e5db52671141f4f04612bcabde28d4c71b89c57
Jake Jarvis <jake@jarv.is>
2025-09-15 14:17:21 -0400

    fix: input types (#1)
    * Update domain-search-form.tsx
    
    * Update header-search.tsx

--- components/domain/header-search.tsx
+++ components/domain/header-search.tsx
@@ -63,7 +63,7 @@ export function HeaderSearch() {
           onKeyDown={(e) => {
             if (e.key === "Enter") submit();
           }}
-          inputMode="url"
+          type="url"
           placeholder="Search any domain"
           className="h-10 sm:h-11 pl-9 pr-14 rounded-xl"
           aria-label="Search domains"
