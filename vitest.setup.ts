c297531ba948bc8a336ae8912d802aa7db0f6124
Jake Jarvis <jake@jarv.is>
2025-09-30 17:30:12 -0400

    Downgrade @testing-library/jest-dom

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
 import { vi } from "vitest";
 
 // Global mocks for analytics capture to avoid network/log noise in tests
